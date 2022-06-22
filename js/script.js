53a0dc99110e00e82f2c88a58585da8caadec57e
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-06-22 12:50:58 +0200

    Add random move options

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ var computerMove;
 computerMove = 'kamień';
 playerMove = 'papier';
 printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!')
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}        
+printMessage('Wylosowana liczba to: ' + randomNumber);
