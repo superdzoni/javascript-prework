a86601ad71441b99e3eb2e08af85d2382262fbe7
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-06-22 13:03:24 +0200

    Add prompt for player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Computer Move
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -17,3 +18,24 @@ if (randomNumber == '3') {
     computerMove = 'nieznany ruch';
 }     
 printMessage('Mój ruch: ' + computerMove);
+
+// Player Move
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';  
+}
+if (playerInput == '3') {
+  playerMove = 'nożyce';  
+} else {
+  playerMove = 'nieznany ruch';  
+}
+printMessage('Twój ruch: ' + playerMove);
